fd6816ba045e4932fabdf0444f1b18f3dce0d36f
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-04-14 19:26:02 +0800

    modified product model

---
+++ src/models/Product.js
@@ -0,0 +1,72 @@
+import { Schema, model, Types } from "mongoose";
+
+const ProductSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Product Name is required"],
+      maxlength: 100,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+      maxlength: 1_000,
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpeg",
+    },
+    category: {
+      type: String,
+      required: [true, "Category is required"],
+      enum: {
+        values: ["office", "kitchen", "bedroom", "confortthing"],
+        message: "{VALUE} is not supported in category field",
+      },
+    },
+    company: {
+      type: String,
+      required: [true, "Company is required"],
+      enum: {
+        values: ["ikea", "motorola", "liddy", "kupssy"],
+        message: "{VALUE} is not supported in company field",
+      },
+    },
+    colors: {
+      type: String,
+      required: [true, "Color is required"],
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    isShippingFree: {
+      type: Boolean,
+      default: false,
+    },
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    createdBy: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("Product", ProductSchema);
