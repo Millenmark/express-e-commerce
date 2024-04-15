9dcd9ba8e6cebc6a937a80836f1c860fa0c123a2
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-04-15 11:25:14 +0800

    added error handler middleware

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,32 @@
+export default (err, req, res, next) => {
+  console.log(err);
+
+  let customError = {
+    // set default
+    statusCode: err.statusCode || 500,
+    message: err.message || "Something went wrong try again later",
+  };
+
+  if (err.name === "ValidationError") {
+    customError.message = Object.values(err.errors).map(
+      (item) => item.message
+    )[0];
+    customError.statusCode = 400;
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.message = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+
+  if (err.name === "CastError") {
+    customError.message = `No item found with id : ${err.value}`;
+    customError.statusCode = 404;
+  }
+
+  return res
+    .status(customError.statusCode)
+    .json({ message: customError.message });
+};
