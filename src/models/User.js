e389ee446701d64c7cb19a513f9d1db47b0a6225
Millen Mark Aquino <83930500+Millenmark@users.noreply.github.com>
2024-04-15 11:25:54 +0800

    created user model

---
+++ src/models/User.js
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+import validator from "validator";
+import bcryptjs from "bcryptjs";
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide an email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcryptjs.compare(password, this.password);
+};
+
+export default model("User", UserSchema);
